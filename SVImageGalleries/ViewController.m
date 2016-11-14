3135712d5cb49c65a83ba0daa0a031a27643fbd3
Suvan <suvanramani@gmail.com>
2016-11-14 11:26:36 -0800

    Add imageViews and scrollview

--- SVImageGalleries/ViewController.m
+++ SVImageGalleries/ViewController.m
@@ -8,7 +8,14 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIScrollViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIScrollView *pageScrollview;
+
+@property (nonatomic, strong) IBOutlet UIImageView *lighthouseFieldView;
+@property (nonatomic, strong) IBOutlet UIImageView *lighthouseNightView;
+@property (nonatomic, strong) IBOutlet UIImageView *lighthouseZoomedView;
+
 
 @end
 
@@ -17,6 +24,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self setUpScrollview];
+}
+
+
+-(void)setUpScrollview {
+    
+    UIImage *lighthouseField = [UIImage imageNamed:@"Lighthouse-in-Field"];
+    UIImage *lighthouseNight = [UIImage imageNamed:@"Lighthouse-night"];
+    UIImage *lighthouseZoomed = [UIImage imageNamed:@"Lighthouse-zoomed"];
+    
+    self.lighthouseFieldView = [[UIImageView alloc] initWithImage:lighthouseField];
+    self.lighthouseNightView = [[UIImageView alloc] initWithImage:lighthouseNight];
+    self.lighthouseZoomedView = [[UIImageView alloc] initWithImage:lighthouseZoomed];
+    
+    [self.pageScrollview addSubview:self.lighthouseFieldView];
+    [self.pageScrollview addSubview:self.lighthouseNightView];
+    [self.pageScrollview addSubview:self.lighthouseZoomedView];
+    
+    
+    
 }
 
 
